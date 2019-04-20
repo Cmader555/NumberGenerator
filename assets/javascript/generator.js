236b2ef17b566985fd3d542725b79c1fc06aad2e
Christian Mader <Cmader555@users.noreply.github.com>
2019-04-19 22:18:44 -0700

    Got button random number to work/give value to btn

--- assets/javascript/generator.js
+++ assets/javascript/generator.js
@@ -2,13 +2,34 @@
 
 //defining variables 
 var wins = 0;
-var losses = 0; 
-var score = 0; 
+var losses = 0;
+var score = 0;
+var randomSmall;
 
 
 
 
 
-
+//random number generator for target
 var randomNumber = Math.floor(Math.random() * 101) + 20;
-console.log(randomNumber); 
+console.log(randomNumber);
+$("#target").text(randomNumber);
+
+
+//random number generator for score, then random# is applied to button class
+$(".crystal").each (function(){
+
+    randomSmall = Math.floor(Math.random() * 11) + 1;
+    console.log(randomSmall);
+    $(this).attr("value", randomSmall);
+
+
+}); 
+
+
+
+
+
+
+
+
